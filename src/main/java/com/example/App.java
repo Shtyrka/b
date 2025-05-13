910d647a68882c911d737b42b3dae3a5483a9089
Tony Sh <128180505+Anatolich911@users.noreply.github.com>
2025-05-13 11:58:52 -0400

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.MyLibrary;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(MyLibrary.sayHello());
+    }
+}
